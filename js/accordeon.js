dfa075c1255d9758ac7b434df72dc191db248cb1
Grigory <grishka5556@gmail.com>
2022-12-14 11:26:57 +0300

    js cover

--- js/accordeon.js
+++ js/accordeon.js
@@ -1,25 +1,20 @@
 const accordeon = () => {
-
-    const accordeonTitle = document.querySelectorAll('[data-name="accordeon-title"]');
+    const accordeonTitle = document.querySelectorAll(
+        '[data-name="accordeon-title"]'
+    )
 
     accordeonTitle.forEach((el) => {
-        el.addEventListener('click', () => {
-
-            el.classList.toggle('active');
-            let content = el.nextElementSibling;
+        el.addEventListener("click", () => {
+            el.classList.toggle("active")
+            let content = el.nextElementSibling
 
             if (content.style.maxHeight) {
-
-                content.style.maxHeight = '';
-
+                content.style.maxHeight = ""
             } else {
-
-                content.style.maxHeight = content.scrollHeight + 'px';
-
-            };
-        });
-    });
-
+                content.style.maxHeight = content.scrollHeight + "px"
+            }
+        })
+    })
 }
 
-accordeon();
+accordeon()
