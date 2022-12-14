00a94817127a3a09bf87361123bf1cd8a7f1a5a7
Grigory <grishka5556@gmail.com>
2022-12-14 11:29:56 +0300

    js edit2

--- js/tabs.js
+++ js/tabs.js
@@ -1,25 +1,21 @@
 const tabs = () => {
-
-    const tabHeaders = document.querySelectorAll('[data-tab]');
-    const contentBoxes = document.querySelectorAll('[data-tab-content]');
+    const tabHeaders = document.querySelectorAll("[data-tab]")
+    const contentBoxes = document.querySelectorAll("[data-tab-content]")
 
     tabHeaders.forEach(function (item) {
-        item.addEventListener('click', function () {
-
-            const noActiv = document.querySelector('.activ');
-            noActiv.classList.remove('activ');
-            this.classList.add('activ');
+        item.addEventListener("click", function () {
+            const noActiv = document.querySelector(".activ")
+            noActiv.classList.remove("activ")
+            this.classList.add("activ")
 
             contentBoxes.forEach(function (item) {
-                item.classList.add('hidden');
-            });
+                item.classList.add("hidden")
+            })
 
-            const contentBox = document.querySelector('#' + this.dataset.tab);
-            contentBox.classList.remove('hidden');
-
+            const contentBox = document.querySelector("#" + this.dataset.tab)
+            contentBox.classList.remove("hidden")
         })
     })
-
 }
 
-tabs();
+tabs()
