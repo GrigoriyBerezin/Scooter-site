feca8bf8c648459fff262b1c9ae95329725be210
Grigory <grishka5556@gmail.com>
2022-12-14 11:28:29 +0300

    js edit

--- js/modal.js
+++ js/modal.js
@@ -1,125 +1,129 @@
 const modal = () => {
+    const popupLinks = document.querySelectorAll(".popup-link")
+    const body = document.querySelector("body")
+    const lockPadding = document.querySelectorAll(".lock-padding")
 
-    const popupLinks = document.querySelectorAll('.popup-link');
-    const body = document.querySelector('body');
-    const lockPadding = document.querySelectorAll('.lock-padding');
+    let unlock = true
 
-    let unlock = true;
-
-    const timeout = 800;
+    const timeout = 800
 
     if (popupLinks.length > 0) {
         for (let index = 0; index < popupLinks.length; index++) {
-            const popupLink = popupLinks[index];
-            popupLink.addEventListener('click', function (e) {
-                const popupName = popupLink.getAttribute('data-href').replace('#', '');
-                const curentPopup = document.getElementById(popupName);
-                popupOpen(curentPopup);
-                e.preventDefault();
-            });
+            const popupLink = popupLinks[index]
+            popupLink.addEventListener("click", function (e) {
+                const popupName = popupLink
+                    .getAttribute("data-href")
+                    .replace("#", "")
+                const curentPopup = document.getElementById(popupName)
+                popupOpen(curentPopup)
+                e.preventDefault()
+            })
         }
     }
-    const popupCloseIcon = document.querySelectorAll('.close-popup');
+    const popupCloseIcon = document.querySelectorAll(".close-popup")
     if (popupCloseIcon.length > 0) {
         for (let index = 0; index < popupCloseIcon.length; index++) {
-            const el = popupCloseIcon[index];
-            el.addEventListener('click', function (e) {
-                popupClose(el.closest('.popup'));
-                e.preventDefault();
+            const el = popupCloseIcon[index]
+            el.addEventListener("click", function (e) {
+                popupClose(el.closest(".popup"))
+                e.preventDefault()
             })
         }
     }
 
     function popupOpen(curentPopup) {
         if (curentPopup && unlock) {
-            const popupActive = document.querySelector('.popup.open');
+            const popupActive = document.querySelector(".popup.open")
             if (popupActive) {
-                popupClose(popupActive, false);
+                popupClose(popupActive, false)
             } else {
-                bodyLock();
+                bodyLock()
             }
-            curentPopup.classList.add('open');
-            curentPopup.addEventListener('click', function (e) {
-                if (!e.target.closest('.popup__content')) {
-                    popupClose(e.target.closest('.popup'));
+            curentPopup.classList.add("open")
+            curentPopup.addEventListener("click", function (e) {
+                if (!e.target.closest(".popup__content")) {
+                    popupClose(e.target.closest(".popup"))
                 }
-            });
+            })
         }
     }
 
     function popupClose(popupActive, doUnlock = true) {
         if (unlock) {
-            popupActive.classList.remove('open');
+            popupActive.classList.remove("open")
             if (doUnlock) {
-                bodyUnLock();
+                bodyUnLock()
             }
         }
     }
 
     function bodyLock() {
-        const lockPaddingValue = window.innerWidth - document.querySelector('.wrapper').offsetWidth + 'px';
+        const lockPaddingValue =
+            window.innerWidth -
+            document.querySelector(".wrapper").offsetWidth +
+            "px"
 
         if (lockPadding.length > 0) {
             for (let index = 0; index < lockPadding.length; index++) {
-                const el = lockPadding[index];
-                el.style.paddingRight = lockPaddingValue;
+                const el = lockPadding[index]
+                el.style.paddingRight = lockPaddingValue
             }
         }
-        body.style.paddingRight = lockPaddingValue;
-        body.classList.add('lock');
+        body.style.paddingRight = lockPaddingValue
+        body.classList.add("lock")
 
-        unlock = false;
+        unlock = false
         setTimeout(function () {
-            unlock = true;
-        }, timeout);
+            unlock = true
+        }, timeout)
     }
 
     function bodyUnLock() {
         setTimeout(function () {
             if (lockPadding.lenght > 0) {
                 for (let index = 0; index < lockPadding.length; index++) {
-                    const el = lockPadding[index];
-                    el.style.paddingRight = '1px';
+                    const el = lockPadding[index]
+                    el.style.paddingRight = "1px"
                 }
             }
-            body.style.paddingRight = '1px';
-            body.classList.remove('lock');
-        }, timeout);
+            body.style.paddingRight = "1px"
+            body.classList.remove("lock")
+        }, timeout)
 
-        unlock = false;
+        unlock = false
         setTimeout(function () {
-            unlock = true;
-        }, timeout);
+            unlock = true
+        }, timeout)
     }
 
-    document.addEventListener('keydown', function (e) {
+    document.addEventListener("keydown", function (e) {
         if (e.which === 27) {
-            const popupActive = document.querySelector('.popup.open');
-            popupClose(popupActive);
+            const popupActive = document.querySelector(".popup.open")
+            popupClose(popupActive)
         }
-    });
+    })
 
-    (function () {
+    ;(function () {
         if (!Element.prototype.closest) {
             Element.prototype.closest = function (css) {
-                var node = this;
+                var node = this
                 while (node) {
-                    if (node.matches(css)) return node;
-                    else node = node.parentElement;
+                    if (node.matches(css)) return node
+                    else node = node.parentElement
                 }
-                return null;
-            };
+                return null
+            }
         }
-    })();
-    (function () {
+    })()
+    ;(function () {
         if (!Element.prototype.matches) {
-            Element.prototype.matches = Element.prototype.matchesSelector ||
+            Element.prototype.matches =
+                Element.prototype.matchesSelector ||
                 Element.prototype.webkitMatchesSelector ||
                 Element.prototype.mozMathesSelector ||
-                Element.prototype.msMatchesSelector;
+                Element.prototype.msMatchesSelector
         }
-    })();
-
+    })()
 }
 
-modal();
+modal()
